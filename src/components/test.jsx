e7d04e5ad1a396079837b74945ac4ea4b7845e02
Nelkeys <nelsonekoh212@gmail.com>
2025-11-21 00:51:21 +0100

    Removed token from console & added vercel.json

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -4,7 +4,7 @@ import { backend } from "../api/client";
 export default function TestToken() {
   const test = async () => {
     const res = await backend.testToken();
-    console.log("Backend response:", res);
+    //console.log("Backend response:", res);
   };
 
   return <button onClick={test}>Test Token</button>;
